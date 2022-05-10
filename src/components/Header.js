87dfa85b10a2f7bbc7ca12b14a7192318b487598
raza212 <yurazar222@gmail.com>
2022-05-10 10:05:49 +0300

    start project

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(props) {
+  return (
+    <header className="header">
+      <img src="logoo.png" alt="logo" />
+    </header>
+  );
+}
+
+export default Header;
